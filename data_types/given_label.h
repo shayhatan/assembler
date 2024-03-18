cf74a74a5905c030f3039b2e31c08000b43bdc5b
Shay Hatan <shayhatan16@gmail.com>
2024-03-18 21:06:51 +0200

    starting to implament given_label

--- data_types/given_label.h
+++ data_types/given_label.h
@@ -3,9 +3,6 @@
 #ifndef MMN14_GIVEN_LABEL_H
 #define MMN14_GIVEN_LABEL_H
 
-typedef struct GivenLabel {
-    char *label_name;
-    int assembly_line;
-} GivenLabel;
+typedef struct GivenLabel_t *GivenLabel;
 
 #endif /*MMN14_GIVEN_LABEL_H*/
