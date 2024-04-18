7957a42282b50e616625b0129df743e1c474f8e4
shay hatan <93487073+shayhatan@users.noreply.github.com>
2024-04-18 20:55:51 +0300

    Create assembler_run.c

---
+++ runs/assembler_run.c
@@ -0,0 +1,56 @@
+#include "assembler_run.h"
+#include <stdio.h>
+#include "../runs/first_run.h"
+#include "../tables/labels_table.h"
+#include "../tables/decode_table.h"
+#include "../runs/second_run.h"
+#include "../tables/externals_table.h"
+#include "../precomplie/precompile.h"
+#include <stdlib.h>
+
+void cleanup(FILE *source_file);
+
+void assemblerRun(char *files[], int index) {
+    char am_file[81] = "";
+
+    if (!preCompile(files, am_file, index))
+        return;
+
+    int run_result = -3; /* undefined */
+
+    FILE *source_file = fopen(am_file, "r");
+
+    if (source_file == NULL)
+        return;
+
+    labelsTableInit();
+    wordsMapInit();
+    externalsMapInit();
+
+    run_result = run(source_file);
+
+    /* first run has finished successfully */
+    if (run_result == OUT_OF_MEMORY) {
+        cleanup(source_file);
+        exit(1);
+    }
+
+    printLabelsTable();
+    fseek(source_file, 0, SEEK_SET);
+    run_result = secondRun(source_file);
+    printWordsMap();
+    printExternals();
+
+    printf("run finished with status %d\n", run_result);
+
+    cleanup(source_file);
+
+}
+
+
+void cleanup(FILE *source_file) {
+    externalsMapDispose();
+    wordsMapDispose();
+    labelsTableDispose();
+    fclose(source_file);
+}
