a1d0127b3a5ac015fd666b9a618fd61140ca57e0
Shay Hatan <shayhatan16@gmail.com>
2024-04-18 22:43:34 +0300

    adding ifndef!

--- runs/first_run.h
+++ runs/first_run.h
@@ -1,11 +1,13 @@
-/*
- Created by User on 17/03/2024.
-*/
+#ifndef FIRST_RUN_H
+#define FIRST_RUN_H
 
 #include "../parsers/parse_types.h"
+#include <stdio.h>
 
 typedef enum {
     STOP, NEXT, ANALYZE_OUT_OF_MEMORY
 } AnalyzeStatus;
 
 ParseResult run(FILE *srcFile);
+
+#endif /* FIRST_RUN_H */
